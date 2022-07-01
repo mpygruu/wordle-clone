b29abf2212fd545825a20902fadd5e2463f63952
mpygru <barkwa67@gmail.com>
2022-07-01 16:11:34 +0200

    Added selecting random word from json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [word, setWord] = useState(null);
+  
+  //useEffect performs after render.
+  useEffect(() => {
+    fetch('http://localhost:3000/wordsList')
+    .then(response => response.json())
+    .then(json => {
+      const randomWord = json[Math.floor(Math.random()*json.length)];
+      setWord(randomWord);
+    })
+  }, [setWord])
+
   return (
     <div className="App">
-      
+      <h1>Wordle Clone</h1>
     </div>
   );
 }
