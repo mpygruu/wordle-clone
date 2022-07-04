0e25bafd4d4392e6af9868f7e3580d307a2a7695
mpygru <barkwa67@gmail.com>
2022-07-04 17:09:28 +0200

    Added showing currently typed word

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Line({ guess }) {
+export default function Line({ guess, currentGuess }) {
 
   if(guess) {
     return (
@@ -8,6 +8,22 @@ export default function Line({ guess }) {
         {guess.map((letter, index) => {
           
           return <div key={index} className={letter.color}>{letter.key}</div>
+        })}
+      </div>
+    )
+  }
+
+  //returned if is the guessing line
+  if(currentGuess) {
+    let letters = currentGuess.split('');
+
+    return (
+      <div className='Line'>
+        {letters.map((letter, index) => {
+          return <div key={index}>{letter}</div>
+        })}
+        {[...Array(5-letters.length)].map((l, index) => {
+          return <div key={index}></div>
         })}
       </div>
     )
