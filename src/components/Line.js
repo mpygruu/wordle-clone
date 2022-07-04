7019e8b6b4319f5d100971f4f210661d7608ac8d
mpygru <barkwa67@gmail.com>
2022-07-04 15:35:08 +0200

    Added Line

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,6 +1,19 @@
 import React from 'react'
 
-export default function Line({ currentGuess }) {
+export default function Line({ guess }) {
+
+  if(guess) {
+    return (
+      <div className='Line'>
+        {guess.map((letter, index) => {
+          
+          return <div key={index} className={letter.color}>{letter.key}</div>
+        })}
+      </div>
+    )
+  }
+
+  //returned if guess has no value (undefined)
   return (
     <div className='Line'>
         <div></div>
