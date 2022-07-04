a420acec33db5d68f9ea51ccc33b876f9fe21fdd
mpygru <barkwa67@gmail.com>
2022-07-04 17:10:21 +0200

    Added currently typed Line in Board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Line from './Line'
 
-export default function Board({ guessedWords, whichLine }) { 
+export default function Board({ currentGuess, guessedWords, whichLine }) { 
   return (
     <div>
       {guessedWords.map((word, index) => {
+        if(whichLine === index) {
+          return <Line key={index} currentGuess={currentGuess} />
+        }
         return <Line key={index} guess={word} />
       })}
     </div>
